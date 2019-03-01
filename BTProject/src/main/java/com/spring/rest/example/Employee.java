67816286874fe9733bc9d5ca27efe04e78fc6459
kirankawade <kirankawade@PNDOSNEHAL.Virtusa.com>
2019-03-01 20:08:21 +0530

    Spring Rest Example commited.

---
+++ BTProject/src/main/java/com/spring/rest/example/Employee.java
@@ -0,0 +1,39 @@
+package com.spring.rest.example;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+
+@Component
+public class Employee implements Serializable {
+
+	
+	private int id;
+	private String name;
+	private Date createdDate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@JsonSerialize(using=DateSerializer.class)
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+}
---
+++ BTProject/src/main/java/com/spring/rest/example/MainApp.java
@@ -0,0 +1,14 @@
+package com.spring.rest.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MainApp.class, args);
+	}
+}
